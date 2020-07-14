0132ec1d4a8caa74865ecabb0b5bf1fd1516b4e5
Zaki udding <zakiuddin321@gmail.com>
2020-07-15 00:11:12 +0530

    added reducer

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
+import { Provider } from "react-redux";
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Signup from "./components/auth/Signup";
 import Login from "./components/auth/Login";
 
-
+import store  from "./store";
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path= '/' component= { Landing }/>
-      <section className='container'>
-        <Switch>
-        <Route exact path = '/login' component = { Login }/>
-        <Route exact path = '/signup' component = { Signup }/>
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
